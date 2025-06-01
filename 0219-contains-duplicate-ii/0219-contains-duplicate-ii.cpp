17ca48dee5618134f59e35765a0c97224487d105
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2025-06-02 00:26:10 +0530

    Time: 81 ms (31.34%), Space: 98.7 MB (43.08%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> hash;
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(hash.find(nums[i]) != hash.end() && i - hash[nums[i]] <= k) {
+                return true;
+            }
+            hash[nums[i]] = i;  // Update last seen index
+        }
+        return false;
+    }
+};
