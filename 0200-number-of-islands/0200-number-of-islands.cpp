0741da596fa0687c8ecae522b6ce3115590c39f6
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2025-06-05 19:17:45 +0530

    Time: 61 ms (11.21%), Space: 25 MB (13.73%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,37 +1,38 @@
 class Solution {
 public:
-    void dfs(int row, int column, vector<vector<char>>& grid,
-             vector<vector<bool>>& visited) {
+    void DFS(int row, int column, vector<vector<char>>& grid, vector<vector<bool>>& visited)
+    {
         visited[row][column] = true;
         int rows = grid.size();
         int columns = grid[0].size();
 
-        // Explore 4 directions (up, down, left, right)
-        vector<int> dr = {-1, 1, 0, 0};
-        vector<int> dc = {0, 0, -1, 1};
+        vector<int> dr = {-1,1,0,0};
+        vector<int> dc = {0,0,-1,1};
 
-        for (int i = 0; i < 4; ++i) {
-            int nr = row + dr[i];
-            int nc = column + dc[i];
-            if (nr >= 0 && nr < rows && nc >= 0 && nc < columns &&
-                !visited[nr][nc] && grid[nr][nc] == '1') {
-                dfs(nr, nc, grid, visited);
+        for(int i=0;i<4;i++)
+        {
+            int nrow = row + dr[i];
+            int ncol = column + dc[i];
+            if(nrow >= 0 && nrow < rows && ncol >= 0 && ncol < columns && 
+            grid[nrow][ncol]=='1' && !visited[nrow][ncol])
+            {
+                DFS(nrow, ncol, grid, visited);
             }
         }
     }
-
     int numIslands(vector<vector<char>>& grid) {
         int rows = grid.size();
         int columns = grid[0].size();
-
-        vector<vector<bool>> visited(rows, vector<bool>(columns, false));
+        vector<vector<bool>> visited(rows,vector<bool>(columns,false));
         int numberOfIslands = 0;
-
-        for (int row = 0; row < rows; row++) {
-            for (int column = 0; column < columns; column++) {
-                if (!visited[row][column] && grid[row][column] == '1') {
+        for(int row = 0;row < rows;row++)
+        {
+            for(int column = 0;column < columns;column++)
+            {
+                if(grid[row][column] == '1' && !visited[row][column])
+                {
                     numberOfIslands++;
-                    dfs(row, column, grid, visited);
+                    DFS(row,column,grid,visited);
                 }
             }
         }
