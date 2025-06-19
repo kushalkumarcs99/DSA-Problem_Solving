35d0c5b3b07208907ab502be0483c2136f1a272d
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2025-06-19 12:49:23 +0530

    Time: 0 ms (100%), Space: 10.8 MB (5.81%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,18 +2,21 @@ class Solution {
 public:
     int robDPHelper(int i, vector<int>& nums, vector<int>& dp)
     {
-        if(i >= nums.size()) return 0;
-        if(dp[i]!= -1) return dp[i];
+        if(i==0) return nums[0];
+        if(i<0) return 0;
 
-        int include = nums[i] + robDPHelper(i+2,nums,dp);
-        int notInclude = robDPHelper(i+1, nums, dp);
+        if(dp[i]!=-1) return dp[i];
+
+        int include = nums[i] + robDPHelper(i-2,nums,dp);
+        int notInclude = robDPHelper(i-1,nums,dp);
 
         dp[i] = max(include, notInclude);
+
         return dp[i];
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n,-1);
-        return robDPHelper(0, nums, dp);
+        return robDPHelper(n-1,nums,dp);
     }
 };
