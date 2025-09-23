64c17dd9544fd9397c6a4092c138a992e9931742
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2025-09-23 12:45:27 +0530

    Time: 71 ms (75.98%), Space: 98.9 MB (15.76%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        int n = nums.size();
+
         unordered_map<int, int> hash;
 
-        for(int i = 0; i < nums.size(); i++) {
-            if(hash.find(nums[i]) != hash.end() && i - hash[nums[i]] <= k) {
+        for(int i=0;i<n;i++)
+        {
+            if(hash.find(nums[i]) != hash.end() && abs(hash[nums[i]] - i)<=k)
+            {
                 return true;
             }
-            hash[nums[i]] = i;  // Update last seen index
+            hash[nums[i]] = i;
         }
         return false;
+
+        
     }
 };
