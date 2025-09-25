1e612f12add34571fa447d40758e12ab81d2350a
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2025-09-25 19:04:11 +0530

    Time: 7 ms (49.3%), Space: 26.9 MB (93.99%) - LeetHub

---
+++ 3463-alternating-groups-i/3463-alternating-groups-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors) {
+        int n = colors.size();
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            int left  = colors[(i - 1 + n) % n];
+            int mid   = colors[i];
+            int right = colors[(i + 1) % n];
+
+            if (mid != left && mid != right) {
+                result++;
+            }
+        }
+        return result;
+    }
+};
