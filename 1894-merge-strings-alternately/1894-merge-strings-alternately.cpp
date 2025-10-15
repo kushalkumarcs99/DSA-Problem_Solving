539369ee1383bab62bf39e0b33d5a5f7e7fc6405
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2025-10-15 17:29:00 +0530

    Time: 4 ms (30.73%), Space: 8.5 MB (62.88%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cpp
@@ -5,7 +5,6 @@ public:
         int m = word2.size();
 
         int i = 0, j = 0;
-        int flag = true;
         string result = "";
          while (i < n || j < m) {
             if (i < n) result += word1[i++];
