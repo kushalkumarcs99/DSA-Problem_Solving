d989779ef516067d33588e84b11d2074a298f5a9
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2025-10-17 19:39:26 +0530

    Time: 3 ms (19.06%), Space: 8.5 MB (83.44%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,22 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int numDecodings(string s) {
         int n = s.size();
-        vector<int> dp(n+1);
-        dp[0] = 1;
-        dp[1] = s[0] == '0' ? 0 : 1;
+        if (n == 0) return 0;
+        // memo[i] = number of ways to decode substring s[i..end-1], -1 means not computed
+        vector<int> memo(n, -1);
+        return dfs(0, s, memo);
+    }
 
-        for(int i=2;i<=n;i++)
-        {
-            int oneDigit = stoi(s.substr(i - 1, 1));
-            int twoDigits = stoi(s.substr(i - 2, 2));
+private:
+    int dfs(int i, const string &s, vector<int> &memo) {
+        int n = s.size();
+        if (i == n) return 1;            // reached end -> one valid decoding
+        if (s[i] == '0') return 0;       // cannot decode a leading '0'
+        if (memo[i] != -1) return memo[i];
 
-            if(oneDigit >= 1)
-                dp[i] += dp[i-1];
+        int ways = 0;
 
-            if(twoDigits >= 10 && twoDigits <= 26)
-                dp[i] += dp[i-2];
+        // take one digit
+        ways += dfs(i + 1, s, memo);
+
+        // take two digits if valid
+        if (i + 1 < n) {
+            int two = (s[i] - '0') * 10 + (s[i + 1] - '0');
+            if (two >= 10 && two <= 26)
+                ways += dfs(i + 2, s, memo);
         }
-        return dp[n];
+
+        memo[i] = ways;
+        return ways;
     }
 };
