5d8296bfdb68e5b0c3d40c12eed8c51704de013b
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2025-10-29 16:31:47 +0530

    Time: 7 ms (77.36%), Space: 34.1 MB (54.34%) - LeetHub

---
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int rows, cols;
+    void dfs(vector<vector<int>>& grid, int row, int col)
+    {
+        if(row < 0 || col < 0 || row >= rows || col >= cols) return;
+        if(grid[row][col] != 1) return;
+
+        grid[row][col] = -1;
+
+        dfs(grid,row+1,col);
+        dfs(grid,row-1,col);
+        dfs(grid,row,col+1);
+        dfs(grid,row,col-1);
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        if(grid.empty()) return 0;
+
+        rows = grid.size();
+        cols = grid[0].size();
+
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                if((i == 0 || j == 0 || i == rows-1 || j == cols-1) && grid[i][j]==1)
+                {
+                    dfs(grid,i,j);
+                }
+            }
+        } 
+
+        int ans = 0;
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]==1) ans++;
+            }
+        }
+
+        return ans;
+    }
+};
