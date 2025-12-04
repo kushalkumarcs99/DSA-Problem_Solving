edb3d828e7ca95c405ab8e6021eb37720003682b
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2025-12-04 12:44:53 +0530

    Time: 4 ms (57.96%), Space: 102.1 MB (40.22%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -4,23 +4,27 @@ public:
         int n = grid.size();
         int m = grid[0].size();
 
-        int result = 0;
+        int islandPerimeter = 0;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == 1) {
-                    result += 4;
-
-                    if (i > 0 && grid[i - 1][j] == 1) {
-                        result -= 2;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    islandPerimeter += 4;
+                    if(i>0 && grid[i-1][j]==1)
+                    {
+                        islandPerimeter -= 2; 
                     }
 
-                    if (j > 0 && grid[i][j - 1] == 1) {
-                        result -= 2;
+                    if(j>0 && grid[i][j-1]==1)
+                    {
+                        islandPerimeter -= 2;
                     }
                 }
             }
         }
-        return result;
+        return islandPerimeter;
     }
 };
