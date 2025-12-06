d05c163f1034ee0c9a23e33fd70b2f7be442561c
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2025-12-06 12:32:53 +0530

    Time: 7 ms (48.73%), Space: 19.1 MB (83.75%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,39 +1,46 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& adj, vector<bool>& visited, vector<bool>& onPath) {
-        visited[node] = true;
-        onPath[node] = true;
-
-        for (int neighbor : adj[node]) {
-            if (!visited[neighbor]) {
-                if (dfs(neighbor, adj, visited, onPath)) return true;
-            } else if (onPath[neighbor]) {
-                // Cycle detected
-                return true;
-            }
-        }
-
-        onPath[node] = false;
-        return false;
-    }
-
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses, 0);
 
-        // Build adjacency list
-        for (const auto& pre : prerequisites) {
-            adj[pre[1]].push_back(pre[0]);
+        for(auto &p : prerequisites)
+        {
+            int node1 = p[0];
+            int node2 = p[1];
+
+            adj[node1].push_back(node2);
+            indegree[node2]++;
         }
 
-        vector<bool> visited(numCourses, false);
-        vector<bool> onPath(numCourses, false);
+        queue<int> q;
 
-        for (int i = 0; i < numCourses; ++i) {
-            if (!visited[i]) {
-                if (dfs(i, adj, visited, onPath)) return false;
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.push(i);
             }
         }
 
-        return true;
+        int processed = 0;
+
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+
+            processed++;
+
+            for(int next : adj[node])
+            {
+                indegree[next]--;
+                if(indegree[next] == 0)
+                {
+                    q.push(next);
+                }
+            }
+        }
+        return processed == numCourses;
     }
 };
