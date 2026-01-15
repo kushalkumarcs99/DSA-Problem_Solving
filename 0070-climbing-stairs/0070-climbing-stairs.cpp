c1c72bcb400b0984cd4ed7a89a5a744935f9b39d
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2026-01-15 23:57:58 +0530

    Time: 0 ms (100%), Space: 8.6 MB (33.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if (n == 1) return 1;  // special case
-        if (n == 2) return 2;
+        if(n==1) return 1;
+        if(n==2) return 2;
         vector<int> dp(n+1);
         dp[1] = 1;
         dp[2] = 2;
@@ -11,6 +11,7 @@ public:
         {
             dp[i] = dp[i-1] + dp[i-2];
         }
+
         return dp[n];
     }
 };
