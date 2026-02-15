afd46a2075203f6819d473c120637a802878063b
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2026-02-16 00:20:59 +0530

    Time: 3 ms (90.45%), Space: 33.1 MB (61.99%) - LeetHub

---
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -0,0 +1,28 @@
+#include <queue>
+#include <vector>
+using namespace std;
+
+class KthLargest {
+public:
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int K;
+
+    KthLargest(int k, vector<int>& nums) {
+        K = k;
+
+        for (int num : nums) {
+            pq.push(num);
+            if (pq.size() > K) {
+                pq.pop();
+            }
+        }
+    }
+    
+    int add(int val) {
+        pq.push(val);
+        if (pq.size() > K) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
