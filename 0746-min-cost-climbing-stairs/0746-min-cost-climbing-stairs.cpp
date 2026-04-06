3e22ef2aa4fd2ee859fb005a4ab7af20de9e00c5
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2026-04-06 12:35:42 +0530

    Time: 1 ms (12.63%), Space: 17.7 MB (28.46%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    int minCostClimbiingStairs(int ind, vector<int>& cost, vector<int>& dp) {
-        if (ind >= cost.size()) {
-            return 0;
-        }
-        if(dp[ind]!=-1) return dp[ind];
-        int include = cost[ind] + min(minCostClimbiingStairs(ind + 1, cost, dp),
-                                      minCostClimbiingStairs(ind + 2, cost, dp));
+    int f(int idx, vector<int>& cost, vector<int> &dp)
+    {
+        if(idx >= cost.size()) return 0;
+        if(dp[idx] != -1) return dp[idx];
+        int stepOne = cost[idx] + f(idx + 1, cost, dp);
+        int stepTwo = cost[idx] + f(idx + 2, cost, dp);
 
-        return dp[ind] = include;
+        return dp[idx] = min(stepOne, stepTwo);
     }
     int minCostClimbingStairs(vector<int>& cost) {
-        vector<int> dp(cost.size(), -1);
-        int result = min(minCostClimbiingStairs(0, cost, dp),
-                         minCostClimbiingStairs(1, cost, dp));
+        int n = cost.size();
+        vector<int> dp(n,-1);
+        int indexZero = f(0,cost,dp);
+        int indexOne = f(1, cost,dp);
 
-        return result;
+        return min(indexZero, indexOne);
     }
 };
