843dc5a31e2f8e2cd388bba99fbbbeba2e7e055f
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2026-04-21 13:56:57 +0530

    Time: 3 ms (87.38%), Space: 8.5 MB (77.26%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,28 +1,17 @@
 class Solution {
 public:
-    int dp[1001][1001];
-    bool isPalindrome(const std::string &str, int left, int right) {
-    if(left >= right) return 1;
-
-    if(dp[left][right] != -1) return dp[left][right];
-
-    if(str[left] == str[right]) return dp[left][right] = isPalindrome(str,left+1,right-1);
-
-    return dp[left][right] = false;
-}
     int countSubstrings(string s) {
         int n = s.length();
-    int maxLen = 0;
-    int longest = 0;
-    memset(dp,-1,sizeof(dp));
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+        // Odd length palindromes (single character center)
+        for (int l = i, r = i; l >= 0 && r < n && s[l] == s[r]; l--, r++)
+            count++;
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = i; j < n; ++j) {
-            if (isPalindrome(s, i, j)) {
-                longest++;
-            }
-        }
+        // Even length palindromes (center between two characters)
+        for (int l = i, r = i + 1; l >= 0 && r < n && s[l] == s[r]; l--, r++)
+            count++;
     }
-    return longest;
+    return count;
     }
 };
