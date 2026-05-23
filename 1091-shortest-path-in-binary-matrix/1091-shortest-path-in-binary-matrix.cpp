0e77f680b3e2d22401345434cfa6f078b4dea946
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2026-05-23 15:30:26 +0530

    Time: 21 ms (39.34%), Space: 25.3 MB (46.44%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,57 +1,42 @@
 class Solution {
 public:
-    int shortestBFS(vector<vector<int>>& grid, vector<vector<int>>& visited) {
-    int n = grid.size();
-    int m = grid[0].size();
-
-    queue<pair<int, int>> q;
-    q.push({0, 0});
-    visited[0][0] = 1;
-
-    int steps = 1; // starting cell counts
-
-    vector<pair<int, int>> dirs = {
-        {1,0},{-1,0},{0,1},{0,-1},
-        {1,1},{1,-1},{-1,1},{-1,-1}
-    };
-
-    while (!q.empty()) {
-        int size = q.size();
-
-        for (int i = 0; i < size; i++) {
-            auto [x, y] = q.front();
-            q.pop();
-
-            // ✅ destination reached
-            if (x == n-1 && y == m-1)
-                return steps;
-
-            for (auto dir : dirs) {
-                int dx = x + dir.first;
-                int dy = y + dir.second;
-
-                if (dx >= 0 && dy >= 0 && dx < n && dy < m &&
-                    grid[dx][dy] == 0 && !visited[dx][dy]) {
-                    
-                    visited[dx][dy] = 1;
-                    q.push({dx, dy});
-                }
-            }
-        }
-
-        steps++; // ✅ increase level AFTER processing all nodes
-    }
-
-    return -1; // not reachable
-}
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         if (grid[0][0] == 1)
             return -1;
-        vector<vector<int>> visited(n, vector<int>(m,0));
-        int result = shortestBFS(grid, visited);
 
-        return result;
+        queue<pair<int, int>> q;
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1},  {0, -1},
+                                       {1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+        q.push({0, 0});
+        vis[0][0] = 1;
+        int length = 1;
+        while (!q.empty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                auto [nx, ny] = q.front();
+                q.pop();
+
+                if (nx == n - 1 && ny == m - 1)
+                    return length;
+                
+                for(auto dir : dirs)
+                {
+                    int dx = nx + dir.first;
+                    int dy = ny + dir.second;
+
+                    if(dx >= 0 && dy >= 0 && dx < n && dy < m && grid[dx][dy] == 0 && !vis[dx][dy])
+                    {
+                        vis[dx][dy] = 1;
+                        q.push({dx, dy});
+                    }
+                }
+            }
+            length++;
+        }
+        return -1;
     }
 };
