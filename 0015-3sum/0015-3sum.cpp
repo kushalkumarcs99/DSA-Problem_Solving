c082d6e74102f338a991b88c99b37dcf35668395
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2026-05-24 17:21:22 +0530

    Time: 51 ms (43.68%), Space: 28.9 MB (99.51%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
         vector<vector<int>> result;
-    sort(nums.begin(), nums.end()); // Sort the array first
+        for(int i=0;i<n-1;i++)
+        {
+            if(i>0 && nums[i] == nums[i-1]) continue;
 
-    for (int i = 0; i < nums.size() - 2; ++i) {
-        // Skip duplicate elements for the first number
-        if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i+1;
+            int right = n-1;
 
-        int left = i + 1;                // Second pointer
-        int right = nums.size() - 1;     // Third pointer
+            while(left < right)
+            {
+                int sum = nums[left] + nums[i] + nums[right];
+                if(sum == 0)
+                {
+                    result.push_back({nums[left], nums[i], nums[right]});
 
-        while (left < right) {
-            int sum = nums[i] + nums[left] + nums[right];
-
-            if (sum == 0) {
-                result.push_back({nums[i], nums[left], nums[right]});
-                // Skip duplicates for the second and third numbers
-                while (left < right && nums[left] == nums[left + 1]) left++;
-                while (left < right && nums[right] == nums[right - 1]) right--;
-                left++; right--;
-            } else if (sum < 0) {
-                left++; // Need a larger sum
-            } else {
-                right--; // Need a smaller sum
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                while(left < right && nums[right] == nums[right-1]) right--;
+                left++, right--;
+                }
+                else if(sum < 0) {left++;}
+                else {right--;}
             }
         }
-    }
-    return result;
+        return result;
     }
 };
