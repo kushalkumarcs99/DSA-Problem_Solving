c98c875a85abc7535093b14beb871cfd803be43f
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2026-05-28 19:36:22 +0530

    Time: 46 ms (69.37%), Space: 29.1 MB (44.76%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -4,6 +4,7 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         vector<vector<int>> result;
+
         for(int i=0;i<n-1;i++)
         {
             if(i>0 && nums[i] == nums[i-1]) continue;
@@ -11,19 +12,21 @@ public:
             int left = i+1;
             int right = n-1;
 
-            while(left < right)
+            while(left<right)
             {
                 int sum = nums[left] + nums[i] + nums[right];
                 if(sum == 0)
                 {
-                    result.push_back({nums[left], nums[i], nums[right]});
+                    result.push_back({nums[left],nums[i],nums[right]});
 
                     while(left < right && nums[left] == nums[left+1]) left++;
-                while(left < right && nums[right] == nums[right-1]) right--;
-                left++, right--;
+                    while(left < right && nums[right] == nums[right-1]) right--;
+
+                    left++;
+                    right--;
                 }
-                else if(sum < 0) {left++;}
-                else {right--;}
+                else if(sum < 0){left++;}
+                else{right--;}
             }
         }
         return result;
