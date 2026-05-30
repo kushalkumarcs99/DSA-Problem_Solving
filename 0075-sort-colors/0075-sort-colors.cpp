159ce0629a7169797893746d59f0e774754092b7
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2026-05-30 19:41:39 +0530

    Time: 0 ms (100%), Space: 11.7 MB (47.63%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int n = nums.size();
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(int i=0;i<n;i++)
+        {
+            pq.push(nums[i]);
+        }
+
+        int  i =0;
+        while(!pq.empty())
+        {
+            nums[i] = pq.top();
+            pq.pop();
+            i++;
+        }
+    }
+};
