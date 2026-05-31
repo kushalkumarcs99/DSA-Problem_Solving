41b68c61491a1ea9d52eddedd1e2854cd03f98c5
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2026-05-31 22:08:21 +0530

    Time: 23 ms (93.28%), Space: 106.7 MB (81.61%) - LeetHub

---
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        int n = asteroids.size();
+
+        sort(asteroids.begin(), asteroids.end());
+
+        long long mass1 = mass;
+
+        for(const int asteroid : asteroids)
+        {
+            if(mass1 < asteroid)
+            {
+                return false;
+            }
+
+            mass1 += asteroid;
+        }
+        return true;
+    }
+};
