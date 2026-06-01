2ffce010325d9b39396101c0268e6d8213beec25
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2026-06-01 10:22:56 +0530

    Time: 9 ms (10.33%), Space: 20.8 MB (73.76%) - LeetHub

---
+++ 0346-moving-average-from-data-stream/0346-moving-average-from-data-stream.cpp
@@ -0,0 +1,26 @@
+class MovingAverage {
+public:
+    int size;
+    vector<int> queue;
+    MovingAverage(int size) {
+        this->size = size;
+    }
+    
+    double next(int val) {
+        queue.push_back(val);
+
+        int windowSum = 0;
+        for(int i = max(0, (int)queue.size()-size); i < queue.size(); i++)
+        {
+            windowSum += queue[i];
+        }
+
+        return windowSum * 1.0 / min((int)queue.size(), size);
+    }
+};
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage* obj = new MovingAverage(size);
+ * double param_1 = obj->next(val);
+ */
