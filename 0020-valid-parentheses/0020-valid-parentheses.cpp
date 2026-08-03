2eb5d2ed58a58ae0a6dd6a6ef428e69c890d1469
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2026-08-03 11:19:05 +0530

    Time: 0 ms (100%), Space: 9 MB (18.26%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
     bool isValid(string s) {
+        unordered_map<char, char> mappings;
+
+        mappings[')'] = '(';
+        mappings[']'] = '[';
+        mappings['}'] = '{';
+
         stack<char> st;
+
         for (char c : s) {
-            if (c == '(' || c == '[' || c == '{') {
+            if (mappings.find(c) == mappings.end()) {
+                // Opening bracket
                 st.push(c);
             } else {
-                if (st.empty()) return false;
-                if ((c == ')' && st.top() == '(') || 
-                    (c == ']' && st.top() == '[') || 
-                    (c == '}' && st.top() == '{')) {
-                    st.pop();
-                } else {
+                // Closing bracket
+                if (st.empty())
                     return false;
-                }
+
+                char topElement = st.top();
+                st.pop();
+
+                if (topElement != mappings[c])
+                    return false;
             }
         }
+
         return st.empty();
     }
 };
