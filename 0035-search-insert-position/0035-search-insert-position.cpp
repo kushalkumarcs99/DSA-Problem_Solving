7ce611363c74c66bc276e6f1b6c5945d29fa0a0c
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2026-08-22 13:53:53 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.5%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,23 +1,7 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int n = nums.size();
-
-        int left = 0, right = n-1;
-        while(left <= right)
-        {
-            int mid = left + (right - left) / 2;
-
-            if(nums[mid] == target){ return mid; }
-
-            else if(nums[mid] < target){
-                left = mid + 1;
-            }
-
-            else{
-                right = mid - 1;
-            }
-        }
-        return left;
+        auto it = lower_bound(nums.begin(), nums.end(), target);
+        return it-nums.begin();
     }
 };
